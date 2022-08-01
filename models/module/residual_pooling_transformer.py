fb07ddf55dd6c0e11aa7ad12337a5c4f07989ad6
DongHappyyy <61820204+DongHappyyy@users.noreply.github.com>
2022-08-01 19:57:14 +0800

    Create residual_pooling_transformer.py

---
+++ models/module/residual_pooling_transformer.py
@@ -0,0 +1,12 @@
+"""
+    the implementation of Residual Pooling Transformer
+"""
+import torch
+import torch.nn as nn
+from timm.models.layers import DropPath, trunc_normal_
+
+
+
+"""
+Waiting for update
+"""
